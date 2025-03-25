343992d6f28aa34e4033552447a3a184abcf5bcd
Your Name <your.email@example.com>
2025-03-25 15:49:04 +0530

    added the google effect

---
+++ src/app/(home-page)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+
+export default function Home() {
+  return (
+    <div>
+      < GoogleGeminiEffect pathLengths={[]} />
+    </div>
+  );
+}
